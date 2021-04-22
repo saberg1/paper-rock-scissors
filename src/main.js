9fb8f34b0318260db361408108118667044244fd
StevenTheBearded <>
2021-04-22 19:44:09 -0400

    add html

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 //Query Selectors
 var mainSection = document.getElementById('mainSection')
+var humanWins = document.getElementById('humanWins')
+var computerWins = document.getElementById('computerWins')
+// var classGame = document.getElementById()
+//
+window.addEventListener('load', loadPage)
 
-window.addEventListener('load', pageLoad)
+function loadPage(){
+  // updateWinCount(num)
+  // console.log('rawr')
+  // renderMainPage()
+  // renderClassicGame()
+}
 
-function pageLoad(){
+function renderMainPage(){
   mainSection.innerHTML = `        
   <section class="gameboard-section" id="gameboardSection">
     <p>Choose your game!</p>
@@ -11,4 +21,20 @@ function pageLoad(){
     <div class='difficult-div gamebox' id='difficultDiv'>DIFFICULT</div>
   </section>
   `
+  updateWinCount(2)
+}
+
+function renderClassicGame(){
+  mainSection.innerHTML = `
+  <p>Choose your fighter!</p>
+  <section class="classic-section" id="classSection">
+    <div class='game-token game-avatar' id='rockAvatar'>rock</div>
+    <div class='game-token paper-avatar' id='paperAvatar'>paper</div>
+    <div class='game-token scissor-avatar' id='scissorAvatar'>scissor</div>
+  </section>
+  `
+}
+
+function updateWinCount(num){
+  humanWins.innerText = num
 }
